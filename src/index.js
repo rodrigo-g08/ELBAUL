6bd9028950a7ded31da7bfd1c213e022c38219f4
Rodrigo Gamero <84432430+rodrigo-g08@users.noreply.github.com>
2025-07-06 19:41:48 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
+require('./database'); // conexión a MongoDB
 const express = require("express");
 const app = express();
 const path = require("path");
-const mongoose = require("mongoose");
 
+// Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Motor de plantillas EJS
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-mongoose.connect("mongodb://mongo-server:27017/elbaul"); // igual a tu docker-compose
-
-// rutas
+// Rutas
 app.use("/productos", require("./routes/productoRoute"));
 
-app.listen(3000, () => console.log("Servidor en http://localhost:3000"));
+// Levantar servidor
+app.listen(3000, () => console.log("🚀 Servidor corriendo en http://localhost:3000"));
