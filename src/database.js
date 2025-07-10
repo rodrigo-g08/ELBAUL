2c712246c8b28a3f3c9416741e7da584f4b699f6
Rodrigo Gamero <84432430+rodrigo-g08@users.noreply.github.com>
2025-07-10 16:26:11 -0500

    Subida del nuevo contenido en la raíz del repositorio

--- src/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost:27017/db_ELBAUL", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("✅ Conectado a MongoDB en puerto 27017"))
-.catch(err => console.error("❌ Error conectando a MongoDB:", err));
